f0ca9ae2bbb5f4a2ee378eefcf08f2f686054123
Pavan K <pavan.k@inkitsolutions.com.au>
2025-12-09 08:48:57 +0000

    oData api call and adding css

--- Card.controller.js
+++ Card.controller.js
@@ -10,76 +10,41 @@ sap.ui.define([
 
 	return Controller.extend("com.winslow.FORMSCARD.Card", {
 		onInit: function () {
-			debugger
-			var that = this;
-			sap.ui.core.BusyIndicator.show(0);
-			$.ajax({
-				url: "https://otcs.g080-test.opentext.cloud/cs/cs/api/v1/auth",
-				type: "POST",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-					"Accept": "application/json"
+			var oModel = new JSONModel({});
+			this.getView().setModel(oModel, "myModel");
+		},
+		onAfterRendering: function () {
+			debugger;
+			var params = new URLSearchParams(window.location.search);
+			var title = params.get("title");
+			title = title ? title.split("/")[0] : "";
+			if (!title) {
+				return; // ✅ stop if no title
+			}
+			console.log("Received Title:", title);
+			this.byId("ProductList").setBusy(true);
+			this.getOwnerComponent().getModel().read("/FetchOTFiles", {
+				urlParameters: {
+					"activity": "Form",
+					"keyword": title
 				},
-				data: {
-					"username": "WinslowSAP",
-					"password": "2lw37Hn63BFe8h0Je19o8TiqsCMpGdZE"
-				},
-				success: function (data) {
+				success: function (oData) {
 					debugger
-					$.ajax({
-						url: "https://otcs.g080-test.opentext.cloud/cs/cs/api/v1/nodes/1273181/nodes?where_type=144",
-						type: "GET",
-						headers: {
-							"Accept": "application/json",
-							"OTCSTicket": data.ticket
-						},
-						success: function (oData) {
-							sap.ui.core.BusyIndicator.hide();
-							console.log(oData.data);
-							var oModel = new JSONModel(oData.data);
-							that.getView().setModel(oModel, "myModel");
-						},
-						error: function (error) {
-							sap.ui.core.BusyIndicator.hide();
-							MessageToast.show(error.message)
-						}
-					});
-				},
-				error: function (error) {
-					sap.ui.core.BusyIndicator.hide();
-					MessageToast.show(error.message)
-				}
+					var aFormData = oData.results || [];
+					var oModel = new sap.ui.model.json.JSONModel(aFormData);
+					this.getView().setModel(oModel, "myModel");
+					this.byId("ProductList").setBusy(false);
+				}.bind(this),
+				error: function (oError) {
+					console.error("Error fetching group ID", oError);
+					this.byId("ProductList").setBusy(false);
+				}.bind(this)
 			});
-
 		},
 		handlePress: function (oEvent) {
-			var sPath = oEvent.getSource().getBindingContext('myModel').getPath();
-			var sSrc = oEvent.getSource().getBindingContext('myModel').getModel().getProperty(sPath).actions.filter(i => i.name === "View as Web Page" || i.name === "View")[0].url
-			sSrc = "https://otcs.g080-test.opentext.cloud" + sSrc;
-			//window.location.href = sSrc;
-			//window.open(sSrc);
-		    sap.m.URLHelper.redirect(sSrc,true)
-			// var oHtml = new sap.ui.core.HTML({
-			// 	content: "<iframe src='" + sSrc + "' width='100%' height='600px' style='border:none;'></iframe>"
-			// });
-			// var oDialog = new Dialog({
-			// 	title: "Document Preview",
-			// 	contentWidth: "80%",
-			// 	contentHeight: "80%",
-			// 	resizable: true,
-			// 	draggable: true,
-			// 	content: [oHtml],
-			// 	beginButton: new Button({
-			// 		text: 'Close',
-			// 		press: function () {
-			// 			oDialog.close();
-			// 		}
-			// 	}),
-			// 	afterClose: function () {
-			// 		oDialog.destroy();
-			// 	}
-			// });
-			// oDialog.open();
+			debugger;
+			var sUrl = oEvent.getSource().getBindingContext('myModel').getObject();
+			sap.m.URLHelper.redirect(sUrl.url, true);
 		}
 	});
 });
